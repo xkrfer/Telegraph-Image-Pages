fd2fda3f7d240e84133ed4f2b4404802cc4e7d7e
xkrfer <xkrfer@gmail.com>
2022-11-01 14:37:49 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <img src="/favicon.svg" class="logo" alt="favicon logo" />
+  </div>
+</template>
+<script setup lang="ts">
+
+</script>
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+</style>
