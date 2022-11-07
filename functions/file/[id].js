e34f592af37d0641fe5e0e9a8156a42fada2bd0a
xkrfer <xkrfer@gmail.com>
2022-11-07 15:29:41 +0800

    test kv

--- functions/file/[id].js
+++ functions/file/[id].js
@@ -3,25 +3,31 @@ export async function onRequest(context) {  // Contents of context object
     const url = new URL(request.url);
     const {MODERATE_CONTENT_API_KEY, IMAGES} = env
     const {pathname, search} = url
-    if (IMAGES) {
+    // if (IMAGES) {
         const src = `/${pathname}${search}`
         const userId = IMAGES.get(src)
         if (!userId) {
             return Response.redirect(url.origin + "/404.png", 302)
         }
-    }
-    const FILE_URL = `https://telegra.ph/${pathname}${search}`
-    if (typeof MODERATE_CONTENT_API_KEY === 'string' && MODERATE_CONTENT_API_KEY !== '') {
-        const res = await fetch(`https://api.moderatecontent.com/moderate?key=${MODERATE_CONTENT_API_KEY}&url=${FILE_URL}`);
-        const response = await res.json()
-        if (response.rating_label === 'adult') {
-            return Response.redirect(url.origin + "/block", 302)
-        }
-    }
-    return fetch(FILE_URL, {
-        method: request.method,
-        headers: request.headers,
-        body: request.body,
-    });
+    // }
+
+    return  new Response(JSON.stringify({
+        userId,
+        pathname,
+        search
+    }))
+    // const FILE_URL = `https://telegra.ph/${pathname}${search}`
+    // if (typeof MODERATE_CONTENT_API_KEY === 'string' && MODERATE_CONTENT_API_KEY !== '') {
+    //     const res = await fetch(`https://api.moderatecontent.com/moderate?key=${MODERATE_CONTENT_API_KEY}&url=${FILE_URL}`);
+    //     const response = await res.json()
+    //     if (response.rating_label === 'adult') {
+    //         return Response.redirect(url.origin + "/block", 302)
+    //     }
+    // }
+    // return fetch(FILE_URL, {
+    //     method: request.method,
+    //     headers: request.headers,
+    //     body: request.body,
+    // });
 }
   
