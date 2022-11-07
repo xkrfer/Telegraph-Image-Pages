f98f85cb17e7279dfd3afda7230a2ce17046513e
xkrfer <xkrfer@gmail.com>
2022-11-07 10:09:09 +0800

    fix

--- functions/file/[id].js
+++ functions/file/[id].js
@@ -6,11 +6,10 @@ export async function onRequest(context) {  // Contents of context object
      const FILE_URL = `https://telegra.ph/${pathname}${search}`
     if(typeof MODERATE_CONTENT_API_KEY === 'string' && MODERATE_CONTENT_API_KEY !== ''){
         const res = await fetch(`https://api.moderatecontent.com/moderate?key=${MODERATE_CONTENT_API_KEY}&url=${FILE_URL}`);
-        const response = res.json()
+        const response = await res.json()
         if(response.rating_label === 'adult'){
             return Response.redirect(url.origin+"/block", 302)
         }
-        return res
     }
     return fetch(FILE_URL, {
         method: request.method,
